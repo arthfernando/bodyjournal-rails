97c9661b207c9091fd7dcd6b197222d25b3e7d07
Arthur <arthur.fernando03@gmail.com>
2025-02-04 16:20:20 -0300

    change create account fields UI

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,12 @@ class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
 
+  before_save :format_birthdate
+
   normalizes :email_address, with: ->(e) { e.strip.downcase }
+
+  private
+  def format_birthdate
+    self.birthdate.to_date.strftime("%d-%m-%Y")
+  end
 end
